ad6a2d6bd18653f31c8f7c12188a390a77420444
witwin12 <kunwin12@gmail.com>
2025-02-13 15:36:31 +0700

    ake basic functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -10,3 +10,26 @@
 #เธอได้ลองโหวต กระเพราไป
 #หลังจากนั้นเธอได้ลองสลับไปดูหน้าผลการตอบ
 #เมื่อเข้าไปจะพบว่ามีคนตอบกระเพราขึ้นมา 1 คน
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+class User_test(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+    def can_connect_to_polls(self):
+        # eing ได้เจอแอพหนึ่งที่สนใจ เป็นแอพเกี่ยวกับ poll เธอจึงได้ลองเข้าไปที่เว็บนั้น โดยได้ลองตรวจสอบว่า urls ของเธอชื่อ polls ใหม
+        self.browser.get("http://localhost:8000")
+        self.assertIn("polls", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text  
+        self.assertIn("polls", header_text)
+
+
+if __name__ == "__main__":  
+     unittest.main()  
+
--- quizw12/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Quizw12Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quizw12'
